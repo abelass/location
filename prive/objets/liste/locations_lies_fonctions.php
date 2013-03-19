8377fd39076dcc11c945dc26ee87ee744fea4a98
abelass <websolutions@mychacra.net>
2013-03-19 10:20:49 +0100

    associer

---
+++ prive/objets/liste/locations_lies_fonctions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fonctions du squelette associé
+ *
+ * @plugin     Location
+ * @copyright  2013
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Location\Fonctions
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// pour initiale et afficher_initiale
+include_spip('prive/objets/liste/auteurs_fonctions');
+
+?>
